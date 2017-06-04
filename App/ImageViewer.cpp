bfba47d9e23c6fa84043aaec0b5362fc37b22a31
Samuel Bostick <me@samuelbostick.com>
2017-06-03 20:58:51 -0400

    feat(fileDialog): Added support for zenity file dialog for linux users.

--- App/ImageViewer.cpp
+++ App/ImageViewer.cpp
@@ -1,16 +1,49 @@
 #include <stdio.h>
+#include <string>
 #include <opencv2/opencv.hpp>
 
 #include "Validate.hpp"
 
 int main(int argc, char** argv){
+    std::string path;
+    bool dialogUsed = false;
+
     if(!Validate::ValidateArgumentCount(argc)){
-        printf("usage: ImageViewer <image_path>\n");
-        return -1;
+        #ifdef linux
+            FILE *file;
+            if(file = popen(
+                "zenity --title=\"Select an image\" --file-selection",
+                "r"
+            )){
+                char buffer[512];
+                std::string selectedPath = "";
+                while(fgets(buffer, sizeof(buffer), file) != NULL){
+                    selectedPath += buffer;
+                }
+                pclose(file);
+
+                selectedPath.erase(std::remove(
+                    selectedPath.begin(),
+                    selectedPath.end(),
+                    '\n'
+                ), selectedPath.end());
+                path = selectedPath;
+                dialogUsed = true;
+            }
+        #endif
+
+        if(!dialogUsed){
+            printf("usage: ImageViewer <image_path>\n");
+            return -1;
+        }
     }
 
     cv::Mat image;
-    image = cv::imread(argv[1], 1);
+    if(dialogUsed){
+        image = cv::imread(path, 1);
+    }else{
+        image = cv::imread(argv[1], 1);
+    }
 
     if(!Validate::ValidateImageData(image)){
         printf("No image data present \n");
